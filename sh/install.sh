60a1d25e79b0a45450052ddf42e322f377514980
0612me <0612me@users.noreply.github.com>
2018-04-05 00:27:45 +0800

    Update install.sh

--- sh/install.sh
+++ sh/install.sh
@@ -1,14 +1,17 @@
-apt-get update && apt-get install -y git unzip screen wget apache2
+apt-get update && apt-get install -y git unzip screen wget apache2 aria2
 wget https://github.com/0612me/a2up.ovz/raw/master/sh/a2.conf -P /root/
 wget https://github.com/0612me/a2up.ovz/raw/master/sh/a2dl-on-complete.sh -p /root/
+chmod a+x /root/a2dl-on-complete.sh
 
 wget https://downloads.rclone.org/rclone-current-linux-amd64.zip
 unzip rclone-current-linux-amd64.zip
 chmod 0755 ./rclone-*/rclone
 cp ./rclone-*/rclone /usr/bin/
 rm -rf ./rclone-*
-echo q | rclone config
-echo 
+echo "/root/.config/rclone/rclone.conf"
 
 git clone https://github.com/binux/yaaw /var/www/html/yaaw
 sed -i 's/:6800/:63001/g' /var/www/html/yaaw/js/yaaw.js
+
+screen -S a2
+aria2c --conf-path=/root/a2.conf
