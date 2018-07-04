b57713a759d3ac4cce12044e3e71af1a902191de
0612me <0612me@users.noreply.github.com>
2018-07-04 21:30:42 +0800

    Create a2f.sh

---
+++ sh/a2f.sh
@@ -0,0 +1,7 @@
+apt-get update && apt-get install -y git unzip screen wget apache2 vim
+
+usermod -s /bin/bash www-data
+chown -R  www-data:www-data /var/www
+git clone https://github.com/binux/yaaw /var/www/html/yaaw
+git clone https://github.com/kalcaddle/KodExplorer /var/www/html/kod
+sed -i 's/:6800/:63005/g' /var/www/html/yaaw/js/yaaw.js
