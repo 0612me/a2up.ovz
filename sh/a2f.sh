3cb33bc7969f9fdbe0fe162c19ad351f282bdc57
0612me <0612me@users.noreply.github.com>
2018-07-04 21:31:58 +0800

    Update a2f.sh

--- sh/a2f.sh
+++ sh/a2f.sh
@@ -1,5 +1,5 @@
 apt-get update && apt-get install -y git unzip screen wget apache2 vim
-
+wget https://github.com/0612me/a2up.ovz/raw/master/sh/a2f.conf -P /root/
 usermod -s /bin/bash www-data
 chown -R  www-data:www-data /var/www
 git clone https://github.com/binux/yaaw /var/www/html/yaaw
