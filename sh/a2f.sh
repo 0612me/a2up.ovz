ddf6cf28814b11971f2f4b93d0e3f8f00c8d7758
0612me <0612me@users.noreply.github.com>
2018-07-04 21:36:38 +0800

    Update a2f.sh

--- sh/a2f.sh
+++ sh/a2f.sh
@@ -1,7 +1,7 @@
 apt-get update && apt-get install -y git unzip screen wget apache2 vim php7.0 libapache2-mod-php7.0 php7.0-curl php7.0-gd php7.0-mbstring
 service apache2 restart
 
-wget https://github.com/0612me/a2up.ovz/raw/master/sh/a2f.conf -P /root/
+wget https://github.com/0612me/a2up.ovz/raw/master/sh/a2f.conf -P /var/www/html/
 usermod -s /bin/bash www-data
 chown -R  www-data:www-data /var/www
 git clone https://github.com/binux/yaaw /var/www/html/yaaw
